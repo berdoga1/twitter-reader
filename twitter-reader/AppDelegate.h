55a0d3f8511e24246928438e50db3abfa18d01d6
Batuhan Erdogan <batuhanerdogan@Batuhans-MacBook-Pro-2.local>
2013-10-23 09:56:14 +0300

    Initial Commit

---
+++ twitter-reader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twitter-reader
+//
+//  Created by Batuhan Erdogan on 23.10.2013.
+//  Copyright (c) 2013 Batuhan Erdogan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ twitter-reader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  twitter-reader
+//
+//  Created by Batuhan Erdogan on 23.10.2013.
+//  Copyright (c) 2013 Batuhan Erdogan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
